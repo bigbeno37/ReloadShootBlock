21970102fcdcf2ee1b7185f141d4afb673ebaa5e
bigbeno37 <bigbeno37@gmail.com>
2018-06-08 23:33:55 +1000

    Implemented basic WebSocket integration

--- client/script.js
+++ client/script.js
@@ -1,7 +1,26 @@
+let server;
+
 $(function() {
-    // TODO: Websocket to server
+    // Connect to the WebSocket server
+    server = new WebSocket("ws://localhost:8080");
+
+    server.onerror = function() {
+        alert("Unable to connect to WebSocket server!");
+    };
+
+    server.onopen = function() {
+        console.log("Made connection to WebSocket Server!")
+    };
+
+    server.onmessage = function(message) {
+        console.log('Server said: ' + message.data);
+    }
 });
 
 function createGame() {
     // TODO: Use websocket to create new lobby
+
+    server.send('something');
+
+    console.log("sent something");
 }
--- server/index.js
+++ server/index.js
@@ -6,3 +6,17 @@ app.use(express.static(__dirname + '/../client/'));
 app.get('/', (req, res) => res.sendFile('index.html', {"root": __dirname + '/../client/'}));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+// Web Socket stuff
+const WebSocket = require('ws');
+
+const wsServer = new WebSocket.Server({ port: 8080 });
+console.log("WebSocket Server started at port 8080");
+
+wsServer.on('connection', (connection) => {
+    connection.on('message', (message) => {
+        console.log(`Received: ${message}`);
+    });
+
+    connection.send('something');
+});
